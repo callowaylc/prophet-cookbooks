8ef7652f1a4c56fab56efa3e47d873e12ebefbc3
christian calloway <ccalloway@cancapital.com>
2014-11-01 19:03:42 -0400

    added hooks cookbook skeleton

---
+++ hooks/metadata.rb
@@ -0,0 +1,8 @@
+name             'hooks'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures hooks'
+long_description 'Installs/Configures hooks'
+version          '0.1.0'
+
